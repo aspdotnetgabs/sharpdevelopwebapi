b4c4b1628a82788016b905f2b7c928bb71ec19a7
Bernard Gabon <hewbertgabon@gmail.com>
2020-10-08 00:20:39 +0800

    Added Song API endpoint

---
+++ SharpDevelopWebApi/Controllers/SongController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SharpDevelopWebApi.Controllers
+{
+	/// <summary>
+	/// Description of SongController.
+	/// </summary>
+	public class SongController : ApiController
+	{
+		[Route("api/song/get1song")]
+		[HttpGet]
+		public IHttpActionResult GetSong()
+		{
+			return Ok(mySong);
+		}
+		
+		[Route("api/song")]
+		[HttpGet]
+		public IHttpActionResult GetSongs()
+		{
+			var songs = new List<Song>();
+			
+			var song1 = new Song();
+			song1.Id = 1;
+			song1.Title = "Show Me The Way To Heart";
+			song1.Artist = "Scott Grimes";
+			song1.Genre = "Ballad";
+			songs.Add(song1);
+			
+			var mySong = new Song();
+			mySong.Id = 2;
+			mySong.Title = "Hello My Love";
+			mySong.Artist = "Westlife";
+			mySong.Genre = "Pop";		
+			songs.Add(mySong);
+			
+			var song2 = new Song
+			{
+				Id = 3,
+				Title = "Back For Good",
+				Artist = "Take That",
+				Genre = "Pop"
+			};
+			songs.Add(song2);
+				
+			return Ok(songs);
+		}
+		
+	}
+}
---
+++ SharpDevelopWebApi/Models/Song.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Gabs
+ * Date: 10/8/2020
+ * Time: 12:00 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+public class Song
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Artist { get; set; }
+	public string Genre { get; set; }
+}
