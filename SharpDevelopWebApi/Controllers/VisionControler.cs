9c1444df85060bc1bd96729379bf823532a938cd
Bernard Gabon <hewbertgabon@gmail.com>
2021-03-06 05:56:23 +0800

    Update VisionControler.cs

--- SharpDevelopWebApi/Controllers/VisionControler.cs
+++ SharpDevelopWebApi/Controllers/VisionControler.cs
@@ -17,50 +17,49 @@ namespace SharpDevelopWebApi.Controllers
 	public class VisionController : ApiController
 	{
 		
-        // Add your Computer Vision subscription key and endpoint
-        string subscriptionKey = "PUT YOUR API KEY HERE";
-        string endpoint = "PUT YOUR COMPUTER VISION API ENDPOINT HERE";
+		// Add your Computer Vision subscription key and endpoint
+		string subscriptionKey = "PUT YOUR API KEY HERE";
+		string endpoint = "PUT YOUR COMPUTER VISION API ENDPOINT HERE";
         
 		// AUTHENTICATE. Creates a Computer Vision client
-        private static ComputerVisionClient Authenticate(string endpoint, string key)
-        {
-            ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-            { 
-            	Endpoint = endpoint
-            };
+		private static ComputerVisionClient Authenticate(string endpoint, string key)
+		{
+			ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key)) 
+			{
+				Endpoint = endpoint
+			};
             
-            return client;
-        }
+			return client;
+		}
 
-        private static string CreateDescription(ImageAnalysis imgAnalysis)
-        {
-        	var imageDesc = string.Empty;
-            foreach (var category in imgAnalysis.Categories)
-            {
-                if (category.Detail != null && category.Detail.Landmarks != null)
-                {
-                    foreach (var landmark in category.Detail.Landmarks)
-                    {
-                    	imageDesc += " I found a landmark " + landmark.Name;
-                    }
-                }
+		private static string CreateDescription(ImageAnalysis imgAnalysis)
+		{
+			var imageDesc = string.Empty;
+			foreach (var category in imgAnalysis.Categories) 
+			{
+				if (category.Detail != null && category.Detail.Landmarks != null) 
+				{
+					foreach (var landmark in category.Detail.Landmarks) {
+						imageDesc += " I found a landmark " + landmark.Name;
+					}
+				}
                 
-                if (category.Detail != null && category.Detail.Celebrities != null)
-                {
-                    foreach (var celeb in category.Detail.Celebrities)
-                    {
-                    	imageDesc += celeb.Name + " is in this photo. ";
-                    }
-                }              
-            }
+				if (category.Detail != null && category.Detail.Celebrities != null) 
+				{
+					foreach (var celeb in category.Detail.Celebrities) 
+					{
+						imageDesc += celeb.Name + " is in this photo. ";
+					}
+				}              
+			}
             
-			foreach (var caption in imgAnalysis.Description.Captions)
-            {
+			foreach (var caption in imgAnalysis.Description.Captions) 
+			{
 				imageDesc += " In summary, the photo is about " + caption.Text;
-            }        
+			}        
 
 			return imageDesc;
-        }
+		}
 		
             
 		[HttpPost]
@@ -72,27 +71,27 @@ namespace SharpDevelopWebApi.Controllers
 			
 			ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
 			
-            // Creating a list that defines the features to be extracted from the image. 
-            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
-	        {
+			// Creating a list that defines the features to be extracted from the image. 
+			List<VisualFeatureTypes> features = new List<VisualFeatureTypes>() 
+			{
 				VisualFeatureTypes.Categories, 
 				VisualFeatureTypes.Description
-	        };
+			};
             
-            // Analyze image
-            using (Stream analyzeImageStream = new MemoryStream(imgByteArray))
-            {
-                // Analyze the local image.
-                ImageAnalysis results = await client.AnalyzeImageInStreamAsync(analyzeImageStream, features);
+			// Analyze image
+			using (Stream analyzeImageStream = new MemoryStream(imgByteArray)) 
+			{
+				// Analyze the local image.
+				ImageAnalysis results = await client.AnalyzeImageInStreamAsync(analyzeImageStream, features);
                 
-                if(asJson)
-                	return Ok(results); 
-               	else
-               	{
-               		var imgDesc = CreateDescription(results);
-               		return Ok(imgDesc);
-               	}
-            }
+				if (asJson)
+					return Ok(results);
+				else 
+				{
+					var imgDesc = CreateDescription(results);
+					return Ok(imgDesc);
+				}
+			}
 		}
 		
      
